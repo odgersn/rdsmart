2b1c6764d2ee00f813f8ff85a0db0bcdf566ba74
Brendan Malone <brendan.malone@sydney.edu.au>
2015-05-13 15:38:00 +1000

    namespace and description alignment

--- code/runDsmart.R
+++ code/runDsmart.R
@@ -14,6 +14,8 @@ compositionFile = "formatted_attribute_table2.txt"
 shp = readOGR(dsn=paste0("./polygons/", shapefile), layer=substr(shapefile, 1, nchar(shapefile) - 4))
 polygons<- shp
 
+
+#########################################################################################################3
 # Load covariates as raster stack
 covariates=stack()
 for(filename in dir(path=paste0(getwd(),"/covariates/"), pattern=".img$"))
@@ -23,6 +25,19 @@ for(filename in dir(path=paste0(getwd(),"/covariates/"), pattern=".img$"))
   covariates=stack(covariates, raster(paste0(getwd(),"/covariates/",filename)))
 }
 
+setwd("/home/brendo/myWork/dsmart/data/covariates")
+fileN<- list.files(getwd(),  pattern="img$", full.names=FALSE)
+files<- list.files(getwd(), pattern='img$',full.names=T)
+
+#Load rasters into memory
+s12<- stack()
+for (i in 1:length(files)){
+d1<- readGDAL(files[i])
+r1<- raster(d1)
+names(r1)<- str_sub(fileN[i],1,-5)
+s12<- stack(s12,r1)}
+
+##################################################################################################################
 # Load map unit composition text file
 composition = read.table(paste0(getwd(), "/polygons/", compositionFile), sep=",", header=TRUE)
 colnames(composition) = c("poly","mapunit","soil_class","proportion")
@@ -45,6 +60,48 @@ lookup <- read.table("classLookupTable.txt",sep=",", header=TRUE)
 
 
 test1<- dsmartR(rLocs= s8, nprob = 3, sepP=TRUE, lookup = lookup,cpus=2, param=nrow(lookup), param2 = nlayers(s8))
+
+
+####Prepare data for inclusion in package
+#composition file
+dsT_composition <- composition
+save(dsT_composition, file= "/home/brendo/myWork/dsmart/rPackage/dsmart/pkg/data/dsT_composition.rda")
+
+#Polygon File
+dsT_polygons <- polygons
+save(dsT_polygons, file= "/home/brendo/myWork/dsmart/rPackage/dsmart/pkg/data/dsT_polygons.rda")
+
+#covariates File
+dsT_covariates <- s12
+save(dsT_covariates, file= "/home/brendo/myWork/dsmart/rPackage/dsmart/pkg/data/dsT_covariates.rda")
+
+#Lookup table
+dsT_lookup <- lookup
+save(dsT_lookup, file= "/home/brendo/myWork/dsmart/rPackage/dsmart/pkg/data/dsT_lookup.rda")
+
+
+#Load dsmart rasters into memory
+fileN<- list.files(getwd(),  pattern="img$", full.names=FALSE)
+files<- list.files(getwd(), pattern='img$',full.names=T)
+
+#Load rasters into memory
+setwd("/home/brendo/myWork/dsmart/data/dsmartOuts/rasters")
+fileN<-list.files(getwd(),  pattern="tif$", full.names=FALSE)
+files<- list.files(getwd(), pattern='tif$',full.names=T)
+files
+s13<- stack()
+for (i in 1:length(files)){
+  d1<- readGDAL(files[i])
+  r1<- raster(d1)
+  names(r1)<- str_sub(fileN[i],1,-5)
+  s13<- stack(s13,r1)}
+
+#covariates File
+dsmartOutMaps <- s13
+save(dsmartOutMaps, file= "/home/brendo/myWork/dsmart/rPackage/dsmart/pkg/data/dsmartOutMaps.rda")
+
+
+
 
 
 
