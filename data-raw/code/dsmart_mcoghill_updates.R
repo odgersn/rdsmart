03f2178315562b037a16373d5e3a3a464efe4278
mcoghill <50680476+mcoghill@users.noreply.github.com>
2021-04-06 00:08:49 -0700

    simplify

--- data-raw/code/dsmart_mcoghill_updates.R
+++ data-raw/code/dsmart_mcoghill_updates.R
@@ -1,20 +1,18 @@
-library(terra)
-library(tidyverse)
-library(mlr3verse)
+# library(terra)
+# library(tidyverse)
+# library(mlr3verse)
 
-load("G:/R/dsmart/data/dalrymple_covariates.rda")
-load("G:/R/dsmart/data/dalrymple_composition.rda")
-load("G:/R/dsmart/data/dalrymple_lookup.rda")
-load("G:/R/dsmart/data/dalrymple_observations.rda")
-load("G:/R/dsmart/data/dalrymple_polygons.rda")
-dalrymple_covariates <- rast(dalrymple_covariates)
-dalrymple_polygons <- vect(dalrymple_polygons)
+data("dalrymple_covariates")
+data("dalrymple_composition")
+data("dalrymple_lookup")
+data("dalrymple_observations")
+data("dalrymple_polygons")
 
-covariates <- dalrymple_covariates
-polygons <- dalrymple_polygons
+covariates <- terra::rast(dalrymple_covariates)
+polygons <- terra::vect(dalrymple_polygons)
 composition <- dalrymple_composition
 rate <- 15
-reals <- 10
+reals <- 3
 observations <- dalrymple_observations
 method.sample = "by_polygon"
 method.allocate = "weighted"
@@ -23,12 +21,12 @@ args.model = NULL
 strata = NULL
 outputdir = getwd()
 stub = NULL
-type = "response"
+type = "prob"
 predict = TRUE
 factors = NULL
 nprob = 3
 
-for(i in list.files(file.path(getwd(), "R"), full.names = TRUE)) source(i)
-for(i in list.files(file.path(getwd(), "src"), full.names = TRUE)) Rcpp::sourceCpp(i)
+# for(i in list.files(file.path(getwd(), "R"), full.names = TRUE)) source(i)
+# for(i in list.files(file.path(getwd(), "src"), full.names = TRUE)) Rcpp::sourceCpp(i)
 
-dsm <- dsmart(covariates, polygons, composition, 15, 10, observations, type = type, stub = type)
+dsm <- dsmart(covariates, polygons, composition, 15, 3, observations, type = type, stub = type)
