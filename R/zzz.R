4edb05c07dedf34ddef6f349289f43b9d7d2c6e5
mcoghill <50680476+mcoghill@users.noreply.github.com>
2021-04-18 20:02:09 -0700

    Add package unload script for C++ functions

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,7 @@
 # .onAttach <- function(libname, pkgname) {
 #   packageStartupMessage("Writing of raster files may fail under GDAL >= 3 and PROJ >= 6! This will be fixed in a future release of the rdsmart package.")
 # }
+
+.onUnload <- function (libpath) {
+  library.dynam.unload("rdsmart", libpath)
+}
