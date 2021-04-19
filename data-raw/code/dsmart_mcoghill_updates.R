1e41bdec52f2dddae1e7e3f798d4378278907d27
mcoghill <50680476+mcoghill@users.noreply.github.com>
2021-04-18 17:38:28 -0700

    Update example

--- data-raw/code/dsmart_mcoghill_updates.R
+++ data-raw/code/dsmart_mcoghill_updates.R
@@ -1,32 +1,34 @@
-# library(terra)
-# library(tidyverse)
-# library(mlr3verse)
-
+# Load datasets
 data("dalrymple_covariates")
 data("dalrymple_composition")
 data("dalrymple_lookup")
 data("dalrymple_observations")
 data("dalrymple_polygons")
 
+# Generate variables for function call
 covariates <- terra::rast(dalrymple_covariates)
 polygons <- terra::vect(dalrymple_polygons)
 composition <- dalrymple_composition
 rate <- 15
 reals <- 3
 observations <- dalrymple_observations
-method.sample = "by_polygon"
-method.allocate = "weighted"
-method.model = NULL
-args.model = NULL
-strata = NULL
-outputdir = getwd()
-stub = NULL
-type = "prob"
-predict = TRUE
-factors = NULL
-nprob = 3
+method.sample <- "by_polygon"
+method.allocate <- "weighted"
+strata <- NULL
+outputdir <- getwd()
+stub <- NULL
+type <- "prob"
+predict <- TRUE
+factors <- NULL
+nprob <- 3
 
-# for(i in list.files(file.path(getwd(), "R"), full.names = TRUE)) source(i)
-# for(i in list.files(file.path(getwd(), "src"), full.names = TRUE)) Rcpp::sourceCpp(i)
+# Generate model parameters. method.model = NULL defaults to C5.0 tree models,
+# and args.model is a named list of model parameters
+method.model <- "classif.C50"
+args.model <- list(CF = 0.5)
 
+# To view model specific parameters:
+mlr3::lrn(method.model)$param_set
+
+# Run DSMART algorithm
 dsm <- dsmart(covariates, polygons, composition, 15, 3, observations, type = type, stub = type)
