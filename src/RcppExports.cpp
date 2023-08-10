b5eb4a0c9e05d1e072e92b32dcc69bc61a632e8f
Nathan Odgers <odgersn@landcareresearch.co.nz>
2023-08-10 15:21:27 +1200

    Rcpp update.

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // order_cpp
 IntegerVector order_cpp(SEXP x, bool decreasing);
 RcppExport SEXP _rdsmart_order_cpp(SEXP xSEXP, SEXP decreasingSEXP) {
